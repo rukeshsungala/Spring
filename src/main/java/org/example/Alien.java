b4f1f2fb6bc891f23da4490cfc6b2f2c81bd3f1e
lakshmi <sungalarukesh.777@gmail.com>
2024-07-18 00:43:37 +0530

    initial

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,7 +1,35 @@
 package org.example;
 
 public class Alien {
+    //int age; using private for setters and getters
+    private int age;
+    private Laptop lap;
+
+    public Alien(){
+        System.out.println("Alien Object Creating");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter called");
+        this.age = age;
+    }
+    /* SetAge is a setter Injection.We are setting the value age.
+    */
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,8 +14,18 @@ public class App
         /*Alien obj = new Alien();
         obj.code();
          */
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // it will create objects in container
         Alien obj = (Alien) context.getBean("alien");
+
+       // obj.setAge(21); // Here Age is set as 21
+        System.out.println(obj.getAge());
         obj.code();
+
+        /* Alien obj1 = (Alien) context.getBean("alien");
+        System.out.println(obj1.age);
+        //obj1.code();
+        //For above two objects are same when the scope is singleton
+
+         */
     }
 }
