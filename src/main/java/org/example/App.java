f3a7ba40ca760d4ab0ea5ed1a68714728e8e0983
lakshmi <sungalarukesh.777@gmail.com>
2024-07-17 20:53:17 +0530

    initial

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien obj = new Alien();
+        obj.code();
+        //System.out.println( "Hello World!" );
+    }
+}
