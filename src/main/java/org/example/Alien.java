39ff24a27637114b8c95448379cea904a06db8ce
lakshmi <sungalarukesh.777@gmail.com>
2024-07-22 09:06:41 +0530

    initial

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -3,33 +3,49 @@ package org.example;
 public class Alien {
     //int age; using private for setters and getters
     private int age;
-    private Laptop lap;
+    private Computer com;
 
     public Alien(){
         System.out.println("Alien Object Creating");
     }
+
+    /*@ConstructorProperties({"age","lap"})
+    public Alien(int age , Computer com) {
+        System.out.println("Parametrised Constructor");
+        this.age = age;
+        this.com = com;
+    }*/
 
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-        System.out.println("setter called");
+       // System.out.println("setter called");
         this.age = age;
     }
     /* SetAge is a setter Injection.We are setting the value age.
     */
 
-    public Laptop getLap() {
+   /* public Laptop getLap() {
         return lap;
     }
 
     public void setLap(Laptop lap) {
+        System.out.println("Latpot Setter");
         this.lap = lap;
+    }*/
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer com) {
+        this.com = com;
     }
 
     public void code(){
         System.out.println("Coding");
-        lap.compile();
+        com.compile();
     }
 }
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -1,11 +1,12 @@
 package org.example;
 
-public class Laptop {
+public class Laptop implements Computer{
     public Laptop(){
         System.out.println("Laptop Object Creation");
     }
 
+    @Override
     public void compile(){
-        System.out.println("compiling");
+        System.out.println("compiling using Laptop");
     }
 }
